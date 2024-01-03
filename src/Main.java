ccf93c2fb64304bfdd5e6330f89c32798bd82f51
Dmitrii Volt <dmitriy.volt@gmail.com>
2024-01-03 17:33:13 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
+
+        // Задача 2
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println("dog + 4 = " + dog);
+        System.out.println("cat + 4 = " + cat);
+        System.out.println("paper + 4 = " + paper);
     }
 }
